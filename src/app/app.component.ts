944a9e15cf8fdb8d49025ee475ac14da6d31ceb1
Aditya-2004-GitHub <22f3001516@ds.study.iitm.ac.in>
2025-01-08 11:56:59 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,9 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router'
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-<<<<<<< HEAD
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 // import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-=======
->>>>>>> 7418a35fa51c97000201aeaaa6b46a4d8fdbc693
 
 @Component({
   selector: 'app-root',
@@ -19,41 +16,8 @@ import { FooterComponent } from "./footer/footer.component";
 })
 export class AppComponent {
   title = 'DummyApi';
-<<<<<<< HEAD
 
   // constructor(private modalService: BsModalService) {}
-=======
-  
-  modalRef?: BsModalRef;
-
-
-  constructor(private http:HttpClient , private modalService: BsModalService){
-    this.userData = JSON.parse(localStorage.getItem("DummyApi")|| '')
-  }
-
-  fName:any=''
-  lName:any=''
-  mobile:any=''
-  isUpdate = false
-  selectIndex:any = ''
-
-  userData:any =[]
-
-  openModal(template: TemplateRef<void>,index:any) {
-
-    this.selectIndex = index
-    this.modalRef = this.modalService.show(template,index);
-    console.log(this.userData[index]);
-    console.log(this.userData);
-  }
-
-  submit(){
-    let user={
-      fName : this.fName,
-      lName : this.lName,
-      mobile : this.mobile
-    }
->>>>>>> 7418a35fa51c97000201aeaaa6b46a4d8fdbc693
   
 
 }
