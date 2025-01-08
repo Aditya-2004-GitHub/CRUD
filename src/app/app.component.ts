eb1eb7fb455783d3be49340ab2ff571f416543f3
Aditya-2004-GitHub <22f3001516@ds.study.iitm.ac.in>
2025-01-08 11:42:05 +0530

    netlify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, TemplateRef } from '@angular/core';
-import { RouterOutlet } from '@angular/router'
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router'
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 // import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule, HeaderComponent, FooterComponent,RouterLink,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,67 +18,6 @@ export class AppComponent {
   title = 'DummyApi';
 
   // constructor(private modalService: BsModalService) {}
-  modalRef?: BsModalRef;
-
-
-  constructor(private http:HttpClient , private modalService: BsModalService){
-    this.userData = JSON.parse(localStorage.getItem("DummyApi")|| '')
-  }
-
-  fName:any=''
-  lName:any=''
-  mobile:any=''
-  isUpdate = false
-  selectIndex:any = ''
-
-  userData:any =[]
-
-  openModal(template: TemplateRef<void>,index:any) {
-
-    // this.isUpdate = true
-    this.selectIndex = index
-    this.modalRef = this.modalService.show(template,index);
-    console.log(this.userData[index]);
-    console.log(this.userData);
-  }
-
-  submit(){
-    let user={
-      fName : this.fName,
-      lName : this.lName,
-      mobile : this.mobile
-    }
   
-    this.userData.push(user);
-    localStorage.setItem("DummyApi",JSON.stringify(this.userData));
-    this.fName=''
-    this.lName=''
-    this.mobile=''
-    console.log(this.userData);    
-  }
-
-  delete(index:any){
-    this.userData.splice(index,1)
-    localStorage.setItem("DummyApi",JSON.stringify(this.userData));
-  }
-
-  edit(index:any){
-    this.isUpdate = true
-    this.selectIndex = index
-    this.fName = this.userData[index].fName
-    this.lName = this.userData[index].lName
-    this.mobile = this.userData[index].mobile
-  }
-
-  update(){
-    this.userData[this.selectIndex].fName = this.fName
-    this.userData[this.selectIndex].lName = this.lName
-    this.userData[this.selectIndex].mobile = this.mobile
-    localStorage.setItem("DummyApi",JSON.stringify(this.userData));
-    this.fName=''
-    this.lName=''
-    this.mobile=''
-    this.isUpdate = false
-  }
 
 }
